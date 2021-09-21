79bf77aba4ed401f5e50341fd530ecbcdb24fab0
Faina-Murashko <fainka18@gmail.com>
2021-09-21 08:52:59 +0300

    editing numbers

--- src/com/lesson2/Homework2/Numbers.java
+++ src/com/lesson2/Homework2/Numbers.java
@@ -2,17 +2,16 @@ package com.lesson2.Homework2;
 import java.util.Arrays;
 public class Numbers {
     public void testNumbers() {
-        int array1[] = new int[]{7, 4, -1, 21, 9};
+
+        int[] array1 = new int[]{7, 4, -1, 21, 9};
         boolean Sort = false;
-        int num;
-        while (Sort) {
-            Sort = true;
-            for (int i = 0; i < array1.length - 1; i++) {
-                if (array1[i] > array1[i + 1]) {
-                    Sort = false;
-                    num = array1[i];
-                    array1[i] = array1[i + 1];
-                    array1[i + 1] = num;
+
+        for (int j = 0; j < array1.length; j++){
+            for (int i = 0; i < array1.length -1 - j; i = i + 1){
+                int max = array1[i];
+                if (max > array1[i + 1]){
+                    array1[i] = array1 [ i + 1];
+                    array1 [i + 1] = max;
                 }
 
             }
