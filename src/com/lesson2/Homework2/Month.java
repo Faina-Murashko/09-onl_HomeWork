354edf581b5b747d696b2f3c13594fa089a19ec0
Faina-Murashko <76451946+Faina-Murashko@users.noreply.github.com>
2021-09-23 11:45:24 +0300

    Update Month.java
    month_else if

--- src/com/lesson2/Homework2/Month.java
+++ src/com/lesson2/Homework2/Month.java
@@ -1,6 +1,6 @@
 package com.lesson2.Homework2;
 
-import java.util.Scanner;
+/*import java.util.Scanner;
 
 public class Month {
     public static void main(String[] args) {
@@ -36,4 +36,41 @@ public class Month {
                 break;
         }
     }
-}
+}*/
+
+import java.util.Random;
+
+public class Month {
+    public static void main(String[] args){
+        String [] month = {"Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"};
+        Random Month = new Random();
+        String monthName = month[Month.nextInt(5)];
+
+
+        if(monthName.equals("Январь"))
+            System.out.println("Вы выбрали месяц Январь");
+        else if (monthName.equals("Февраль"))
+            System.out.println("Вы выбрали месяц Январь");
+        else if (monthName.equals("Март"))
+            System.out.println("Вы выбрали месяц Март");
+        else if (monthName.equals("Апрель"))
+            System.out.println("Вы выбрали месяц Арель");
+        else if (monthName.equals("Май"))
+            System.out.println("Вы выбрали месяц Май");
+        else if (monthName.equals("Июнь"))
+            System.out.println("Вы выбрали месяц Июнь");
+        else if (monthName.equals("Июль"))
+            System.out.println("Вы выбрали месяц Июль");
+        else if (monthName.equals("Август"))
+            System.out.println("Вы выбрали месяц Август");
+        else if (monthName.equals("Сентябрь"))
+            System.out.println("Вы выбрали месяц Сентябрь");
+        else if (monthName.equals("Октябрь"))
+            System.out.println("Вы выбрали месяц Октябрь");
+        else if (monthName.equals("Ноябрь"))
+            System.out.println("Вы выбрали месяц Ноябрь");
+        else if (monthName.equals("Декабрь"))
+            System.out.println("Вы выбрали месяц Декабрь");
+        else {
+            System.out.println("Выберите месяц");
+        }
